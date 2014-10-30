2d7b27e438010dd8d7ce68e8f59a1e467c2a0933
valetedo <vale_tedo@libero.it>
2014-10-30 17:28:59 +0100

    navbar&footer

--- assets/test/modules/Group-test.js
+++ assets/test/modules/Group-test.js
@@ -5,7 +5,7 @@ var Group = xtens.module("group");
 
 describe('Group.Views.Edit',function (){
      beforeEach(function(){
-        this.group = new Group.Model({name:"pippo"});
+        this.group = new Group.Model({name:"pippo",id:15});
       
 
      });
@@ -13,12 +13,18 @@ describe('Group.Views.Edit',function (){
      describe('#initialize()',function(){
      beforeEach(function(){
          this.view= new Group.Views.Edit({id:this.group.id});
-         this.view.render(this.view);
+         this.view.render(this.group);
      });
  
      it('should be an initialized object', function() {
             this.view.should.be.an('object');
         });
+     
+     it('should show name property with the right value',function(){
+        var that = this;
+        console.log(that.view);
+        expect(that.view.$(name).val()).to.equal(that.group.get("name"));
+     });
     
      
      });
