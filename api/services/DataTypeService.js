03650b6db8c0af16d3c3db59810492ea9c06ed18
zigur <massimorgon@gmail.com>
2015-01-15 15:58:25 +0100

    new functions for data generation

--- api/services/DataTypeService.js
+++ api/services/DataTypeService.js
@@ -1,8 +1,10 @@
-/**
+/** 
+ *  @module
  *  @author Massimiliano Izzo
  */
+var Constants = sails.config.xtens.constants;
 var DataTypeService = {
-    
+
     getChildrenRecursive: function(parents) {
         parents.forEach(function(parent, index) {
             DataType.find({parent: parent.id}).populate('children').then(function(children) {
@@ -13,7 +15,7 @@ var DataTypeService = {
             });
         });
     },
-    
+
     /**
      * @method
      * @name get
@@ -57,7 +59,46 @@ var DataTypeService = {
                 next(null, result.rows);
             }
         });
+    },
+
+    /**
+     * @method
+     * @name getFlattenedFields
+     * @description flattens the metadata schema returning a 1D array containing all the metadata fields
+     * @param {DataType} dataType - the DataType record whose schema is to flatten
+     * @param {boolean} skipFieldsWithinLoops - if true skips all the metadatafields that are contained within metadata loops
+     */
+
+    getFlattenedFields: function(dataType, skipFieldsWithinLoops) {
+        var flattened = [];
+        var body = dataType.schema && dataType.schema.body;
+
+        // if no body return an empty array
+        if (!body) return flattened;
+
+        // iterate through all groups within the body
+        for (var i=0, len=body.length; i<len; i++) {
+            var groupContent = body[i] && body[i].content;
+
+            // iterate through all the fields/loops
+            for (var j=0, l=groupContent.length; j<l; j++) {
+                if (groupContent[j].label === Constants.METADATA_FIELD) {
+                    flattened.push(groupContent[j]);
+                }
+                else if (groupContent[j].label === Constants.METADATA_LOOP && !skipFieldsWithinLoops) {
+                    var loopContent = groupContent[j] && groupContent[j].content;
+                    for (var k=0; k<loopContent.length; k++) {
+                        if (loopContent[k].label === Constants.METADATA_FIELD) {
+                            flattened.push(loopContent[k]);
+                        }
+                    }
+                }
+
+            }
+        }
+        return flattened;
     }
+
 
 };
 module.exports = DataTypeService;
