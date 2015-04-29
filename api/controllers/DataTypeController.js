524ad0e64195062145cf11e60a84daf712d6433f
valetedo <vale_tedo@libero.it>
2015-04-29 16:52:44 +0200

    added data graph creation

--- api/controllers/DataTypeController.js
+++ api/controllers/DataTypeController.js
@@ -4,36 +4,113 @@
  * @description :: Server-side logic for managing datatypes
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var transactionHandler = sails.config.xtens.transactionHandler;
+
 var DataTypeController = {
-    /*
-create: function(req, res) {
-DataType.find()
-.then(function(dataTypes) {
-res.send(dataTypes);
-}).fail(function(err) {
-res.send(500, {error: "Database Error"});
-});
-},
 
-find: function(req, res) {
+    /**
+     * GET /dataType
+     * GET /dataType/find
+     *
+     * @method
+     * @name find
+     * @description Find dataTypes based on criteria
+     */
+    find: function(req, res) {
 
-},
+        console.log("DataType.find - prova");
 
-findOne: function(req, res) {
+        var query = DataType.find()
+        .where(QueryService.parseCriteria(req))
+        .limit(QueryService.parseLimit(req))
+        .skip(QueryService.parseSkip(req))
+        .sort(QueryService.parseSort(req)).populate('parents');
 
-},
+        query.then(function(dataTypes) {
+            res.json(dataTypes);
+        })
+        .catch(function(err) {
+            return res.serverError(err);
+        });
+    },
 
-update: function(req, res) {
+    /**
+     * POST /dataType
+     * @method
+     * @name create
+     */
+    create: function(req, res) {
+        console.log("DataType.create - prova");
 
-},
+        var dataType = req.body;
+        if(!dataType.schema) {
+            return res.badRequest("a DataType requires at least valid JSON schema");
+        }
+        if (!dataType.name) dataType.name = dataType.schema.name;
+        if (!dataType.model) dataType.model = dataType.schema.model;
+        // omit all the properties relative to associations
+        // var newDataType = _.omit(req.body, ['parents', 'children', 'datas', 'groups']);
+        // var parents = req.param('parents');
 
-updateSelective: function(req, res) {
+        transactionHandler.createDataType(dataType).then(function(idDataType) {
+            return DataType.findOne(idDataType).populate('parents');
+        })
+        .then(function(dataType) {
+            return res.json(dataType);
+        })
+        .catch(function(error) {
+            return res.serverError(error.message);
+        });
 
-},
+        /*
+           DataType.create(newDataType)
 
-destroy: function(req, res) {
+        // if the dataType is successfully created, create all the 
+        .then(function(dataType) {
+        newDataType = dataType;
+        return BluebirdPromise.map(parents, function(parent) {
+        console.log('Associating new DataType ', dataType.name ,'with Parent ', parent);
+        dataType.parents.add(parent);
+        return dataType.save();
+        });
+        })
 
-} */
+        .then(function() {
+        return DataType.find(newDataType.id).populate('parents');
+        })
+
+        .then(function(dataType) {
+        return res.json(dataType);
+        })
+
+        .catch(function(error) {
+        return res.serverError(error);
+        }); */
+
+    },
+
+    /**
+     * PUT /dataType/:id
+     * @method
+     * @name update
+     */
+    update: function(req, res) {
+        var dataType = req.body;
+
+        transactionHandler.updateDataType(dataType).then(function(idDataType) {
+            return DataType.findOne(idDataType).populate('parents');
+        })
+        .then(function(dataType) {
+            return res.json(dataType);
+        })
+        .catch(function(error) {
+            return res.serverError(error.message);
+        });
+    },
+
+    /**
+     * @deprecated
+     */
 
     buildHierarchy: function(req, res) {
         DataType.find({ parent: null}).populate('children').then(function(roots) {
@@ -76,35 +153,51 @@ get_roots: function(next) {
 DataType.find({ parent: null}).populate('children').exec(next);
 }  
 }); */
-},
+    },
 
-        buildGraph : function(req,res) {
-            
-            console.log(req.param("idDataType"));
-            var name = req.param("idDataType");
-           return DataType.findOne({name:name}).then(function(result){
+    /**
+     * @method
+     * @name buildGraph
+     * @description generate and visualize the datatype graph given a root datatype.
+     */
+
+    buildGraph : function(req,res) {
+
+        console.log(req.param("idDataType"));
+        var name = req.param("idDataType");
+        return DataType.findOne({name:name}).then(function(result) {
             var id = result.id;
-            var template = result.classTemplate;
-           DataType.query('WITH RECURSIVE nodes(parentId, parentName,parentTemplate,childId, childName,childTemplate, path, depth) AS (select r.datatype_parents,p1.name,p1.class_template,r.datatype_children,p2.name,p2.class_template,ARRAy[r.datatype_parents],1 from datatype_children__datatype_parents as r, data_type as p1,data_type as p2 where r.datatype_parents = '+ id+' and p1.id = r.datatype_parents and p2.id = r.datatype_children union all select r.datatype_parents,p1.name,p1.class_template,r.datatype_children,p2.name,p2.class_template,path || r.datatype_parents, nd.depth + 1 from datatype_children__datatype_parents as r,data_type as p1,data_type as p2,nodes as nd where r.datatype_parents = nd.childId and p1.id = r.datatype_parents and p2.id = r.datatype_children ) select * from nodes;',function(err,resp){
-                //var nodes = [];
+            var template = result.model;
+            // This query returns the parent-child associations among the datatypes
+            DataType.query('WITH RECURSIVE nodes(parentId, parentName,parentTemplate,childId, childName,childTemplate, path, depth) AS (select r.datatype_children,p1.name,p1.model,r.datatype_parents,p2.name,p2.model,ARRAy[r.datatype_parents],1 from datatype_children__datatype_parents as r, data_type as p1,data_type as p2 where r.datatype_children = $1 and p1.id = r.datatype_children and p2.id = r.datatype_parents union all select r.datatype_children,p1.name,p1.model,r.datatype_parents,p2.name,p2.model,path || r.datatype_children, nd.depth + 1 from datatype_children__datatype_parents as r,data_type as p1,data_type as p2,nodes as nd where r.datatype_children = nd.childId and p1.id = r.datatype_children and p2.id = r.datatype_parents ) select * from nodes;',[id], function(err,resp) {
+
                 var links = [];
-                if(resp.rows.length === 0){
-                links.push({'source':name,'depth':0 ,'target':null,'source_template':template,'target_template':null});
+
+                // if there aren't children do not print any link
+                if(resp.rows.length === 0) {
+                    links.push({
+                        'source':name,'depth':0 ,'target':null,'source_template':template,'target_template':null
+                    });
                 }
-            for(var i =0;i<resp.rows.length;i++){
-            // nodes.push({'id':resp.rows[i].parentid,'name':resp.rows[i].parentname,'template':resp.rows[i].parenttemplate});
-            // nodes.push({'id':resp.rows[i].childid, 'name':resp.rows[i].childname,'template':resp.rows[i].childtemplate});
-             links.push({'source':resp.rows[i].parentname,'target':resp.rows[i].childname,'depth':resp.rows[i].depth,'source_template':resp.rows[i].parenttemplate,'target_template':resp.rows[i].childtemplate });  
-            }
-           // nodes = _.uniq(nodes,'name');
-            //var json = {'nodes':nodes,'links':links};
-            var json = {'links':links};
-            console.log(json);
-            return res.json(json);
+                // populate the links array
+                for(var i =0;i<resp.rows.length;i++) {            
+                    links.push({
+                        'source':resp.rows[i].parentname,
+                        'target':resp.rows[i].childname,
+                        'depth':resp.rows[i].depth,
+                        'source_template':resp.rows[i].parenttemplate,
+                        'target_template':resp.rows[i].childtemplate 
+                    });  
+                }
+
+                var json = {'links':links};
+                console.log(json);
+                return res.json(json);
             });  
         });      
-        }
+    }
 
-        };
 
-        module.exports = DataTypeController;
+};
+
+module.exports = DataTypeController;
