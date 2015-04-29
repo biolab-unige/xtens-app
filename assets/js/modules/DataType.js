24fb981760e82ff746e67ad5f82d25cda6c56f9d
valetedo <vale_tedo@libero.it>
2015-04-29 17:06:50 +0200

    added datatype graph visualization

--- assets/js/modules/DataType.js
+++ assets/js/modules/DataType.js
@@ -35,7 +35,7 @@
         urlRoot: '/dataType',
 
         defaults: {
-            classTemplate: DataTypeClasses.GENERIC
+            model: DataTypeClasses.DATA
         },
 
         /**
@@ -150,11 +150,11 @@
         },
 
         bindings: {
-            '#schemaName': {
+            '#name': {
                 observe: 'name'
             },
-            '#classTemplate': {
-                observe: 'classTemplate',
+            '#model': {
+                observe: 'model',
                 selectOptions: {
                     collection: function() {
                         var coll = [];
@@ -182,6 +182,7 @@
                 getVal: function($el, ev, options) {
                     return $el.val().map(function(value) {
                         return _.findWhere(options.view.existingDataTypes, {id: parseInt(value)});
+                        // return _.parseInt(value);
                     });
                 },
                 onGet: function(vals, options) {
@@ -257,12 +258,23 @@ this.setValidate(); //TODO
             return metadataBody;
         },
 
+
         saveDataType: function(ev) {
             var id = $('#id').val();
             var header = this.$("#schemaHeader").find("select, input, textarea").serializeObject();
             header.fileUpload = header.fileUpload ? true : false;
             var body = this.serialize();
-            var dataTypeDetails = { id: id, name: header.schemaName, schema: {header: header, body: body} };
+            var dataTypeDetails = { 
+                id: id, 
+                name: header.name,
+                schema: {
+                    header: _.omit(header, ['parents']), // parent-child many-to-many associations are not currently saved in the JSON schema 
+                    body: body
+                } 
+            };
+
+            this.model.set("parents", _.pluck(this.model.get("parents"),'id'));
+
             //var dataType = new DataType.Model();
             this.model.save(dataTypeDetails, {
                 //  patch: true,
@@ -353,113 +365,171 @@ this.setValidate(); //TODO
         },
 
         createGraph: function() {
-            var idDatatype = document.getElementById('select1').value;
+            var nameDatatype = document.getElementById('select1').value;
 
             $.post( '/graph',
-                   {idDataType: idDatatype},
+                   {idDataType: nameDatatype},
                    function (err,res,body) {
-                       var graph = body.responseJSON;
 
-                       var links = graph.links;
-                       var nodes = {};
-                       var force;
-                       var width = 960,
-                       height = 500;
+                       d3.select("svg")
+                       .remove();                    
 
-                       var position = width/2;
-                       var depth1;
+                       var margin = {top: 40, right: 120, bottom: 40, left: 120},
+                       width = 960 - margin.left - margin.right,
+                       height = 800 - margin.top - margin.bottom;
 
-                       links.forEach(function(link) {
+                       var tree = d3.layout.tree()
+                       .size([height, width]);
 
-                           link.source = nodes[link.source] || (nodes[link.source] = {name: link.source,template:link.source_template,x:width/2,y:40,fixed:true});
-
-                           if(depth1 === link.depth){
-                               position = position+160;
-                           }
-                           else{
-                               position = link.source.x;
-                           }
-
-                           link.target = nodes[link.target] || (nodes[link.target] = {name: link.target,template:link.target_template,x:position, y:link.depth*100,fixed:true});
-
-                           depth1 = link.depth;
-
-                       });
-
-
-                       force = d3.layout.force()
-                       .nodes(d3.values(nodes).filter(function(d){ return d.name;}))
-                       .links(links)
-                       .size([width, height])
-                       .linkDistance(120)
-                       .charge(-300)
-                       .on("tick", tick)
-                       .start();
-
-                       var svg = d3.select("#main").append("svg")
-                       .attr("width", width)
-                       .attr("height", height);
-
-                       svg.append("defs").selectAll("marker")
-                       .data(["suit"])
-                       .enter().append("marker")
-                       .attr("id", function(d) { return d; })
-                       .attr("viewBox", "0 -5 10 10")
-                       .attr("refX", 15)
-                       .attr("refY", -1.5)
-                       .attr("markerWidth", 6)
-                       .attr("markerHeight",6)
-                       .attr("orient", "auto")
-                       .append("path")
-                       .attr("d", "M0,-5L10,0L0,5");
-
-                       var path = svg.append("g").selectAll("path")
-                       .data(force.links())
-                       .enter().append("path")
-                       .attr("class","link")
-                       .attr("marker-end","url(#suit)");
-
-                       var ellipse = svg.append("g").selectAll("ellipse")
-                       .data(force.nodes())
-                       .enter().append("ellipse")
-                       .attr("rx", 70)
-                       .attr("ry",20)
-                       .call(force.drag);
-
-                       var text1 = svg.append("g").selectAll("text")
-                       .data(force.nodes())
-                       .enter().append("text")
-                       .attr("x", -30)
-                       .attr("y", -5)
-                       .text(function(d) { return d.name;});
-
-                       var text2 =svg.append("g").selectAll("text").data(force.nodes()).enter().append("text")
-                       .attr("x",-30)
-                       .attr("y",10)
-                       .text(function(d){return d.template;});
-
-                       function tick() {
-                           path.attr("d", linkArc);
-                           ellipse.attr("transform",transform);
-                           text1.attr("transform", transform);
-                           text2.attr("transform",transform);
+                       var diagonal = d3.svg.diagonal()
+                       .projection(function(d) { 
+                           return [d.x, d.y-39];
                        }
+                                  );
 
-                       function linkArc(d) {
-                           if(d.target.x && d.target.y){
-                               var dx = d.target.x - d.source.x,
-                               dy = d.target.y - d.source.y,
-                               dr = Math.sqrt(dx * dx + dy * dy);
-                               return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
-                           }
-                           else {
-                               return null;
-                           }
-                       }
+                                  var svg = d3.select("#main").append("svg")
+                                  .attr("width", width + margin.left + margin.right)
+                                  .attr("height", height + margin.top + margin.bottom)
+                                  .append("g")
+                                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-                       function transform(d) {
-                           return "translate(" + d.x + "," + d.y + ")";
-                       }
+                                  var graph = body.responseJSON;
+
+                                  var links = graph.links;
+                                  var nodesByName = {};
+
+                                  console.log(links);
+
+                                  // Create nodes for each unique source and target.
+                                  links.forEach(function(link) {
+                                      var parent,child;
+                                      if (link.target !== null){
+                                          parent = link.source = nodeByName(link.source.toUpperCase() +' '+ link.source_template.toLowerCase() );
+                                          child = link.target = nodeByName(link.target.toUpperCase()+' '+link.target_template.toLowerCase());
+                                      }
+                                      else{
+                                          parent = link.source = nodeByName(link.source.toUpperCase() +' '+ link.source_template.toLowerCase() );
+                                          child = link.target = nodeByName(link.target);
+                                      }
+                                      if (parent.children) parent.children.push(child);
+                                      else parent.children = [child];
+
+
+
+                                  }
+                                               );
+
+                                               var index;
+
+                                               for(var i=0;i<links.length;i++){
+                                                   if(links[i].source.name === nameDatatype ){
+                                                       index = i;
+                                                   }
+                                               }
+                                               var nodes = tree.nodes(links[0].source);
+                                               nodes =_.uniq(nodes,'name');
+
+                                               console.log(nodes);
+
+                                               nodes.forEach(function(d){ d.y= d.depth*150;
+                                                             if(d.x>1000 && d.x<100000){
+                                                                 d.x=d.x/80 -100;
+                                                             }
+                                                             else if(d.x>=100000 && d.x<1000000000){
+                                                                 d.x=d.x/200000 - 120;
+                                                             }
+                                                             else if(d.x >=1000000000){
+                                                                 d.x=d.x/2000000000000 -40;
+                                                             }
+                                                             else if(d.x>500 && d.x <1000){
+                                                                 d.x=(d.x/1.5)*3 -500;
+                                                             }
+                                               });
+
+                                               svg.append("svg:defs").selectAll("marker")
+                                               .data(links)
+                                               .enter().append("svg:marker")
+                                               .attr("id","arrowhead")
+                                               .attr("viewBox", "0 -5 10 10")
+                                               .attr("refX",10)
+                                               .attr("refY",1.5)
+                                               .attr("markerWidth", 5)
+                                               .attr("markerHeight", 5)
+                                               .attr("orient","auto")
+                                               .attr("stroke","grey")
+                                               .attr("stroke-width",10)
+                                               .append("path")
+                                               .attr("d","M0,0L100,100,200,200");
+
+
+                                               svg.selectAll(".link")
+                                               .data(links.filter(function(d){return d.target.name;}))
+                                               .enter().append("path")
+                                               .attr("class", "link")
+                                               .attr("marker-end","url(#arrowhead)")
+                                               .attr("d",diagonal);
+
+                                               svg.selectAll(".node")
+                                               .data(nodes.filter(function(d){return d.name;}))
+                                               .enter().append("ellipse")
+                                               .attr("id",function(d){
+                                                   var arr = d.name.split(" ");
+                                                   return arr[arr.length-1];
+                                               })
+                                               .attr("class", "node")
+                                               .attr("rx",54)
+                                               .attr("ry",39)
+                                               .attr("cx", function(d) { return d.x; })
+                                               .attr("cy", function(d) { return d.y; });
+
+                                               svg.append("g").selectAll("text")
+                                               .data(nodes)
+                                               .enter().append("text")
+                                               .each(function (d) {
+                                                   if(d.name !== null){
+                                                       var arr = d.name.split(" ");
+                                                       if (arr !== undefined) {
+                                                           for (var i = 0; i < arr.length; i++) {
+                                                               d3.select(this).append("tspan")
+                                                               .text(arr[i])
+                                                               .attr("y", function(d){
+                                                                   if(i === arr.length -123942){
+                                                                       return d.y -6*(arr.length-1) +(12)*i+6;
+
+                                                                   }
+                                                                   return d.y -6*(arr.length-1) +(12)*i+3;})
+                                                                   .attr("x", function(d){
+                                                                       return d.x;
+                                                                   } )
+                                                                   .attr("id",function(d){
+                                                                       if( i=== arr.length-1){
+                                                                           return arr[i];
+                                                                       }
+                                                                   })   
+                                                                   .style("font-size","12px")
+                                                                   .style("font-weight",function(){
+
+                                                                       if(i!== arr.length-1){
+                                                                           return "bold";
+                                                                       }
+                                                                   })
+                                                                   .attr("text-anchor","middle")
+                                                                   .attr("class", "tspan"+i);
+                                                           }
+                                                       }
+                                                   }
+                                               });
+
+
+
+
+
+
+                                               function nodeByName(name) {
+                                                   return nodesByName[name] || (nodesByName[name] = {name: name});
+                                               }
+
+
                    }
                   ).fail(function(res){
                       alert("Error: " + res.responseJSON.error);
