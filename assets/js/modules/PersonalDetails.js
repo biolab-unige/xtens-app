f2ad7edacecd8e0ff91dfdb00def5f41618b917c
zigur <massimorgon@gmail.com>
2016-01-27 18:43:12 +0000

    implemented dates with moment and locale fix #52

--- assets/js/modules/PersonalDetails.js
+++ assets/js/modules/PersonalDetails.js
@@ -1,5 +1,5 @@
 (function(xtens, PersonalDetails) {
-    
+
     var i18n = xtens.module("i18n").en;
 
     PersonalDetails.Model = Backbone.Model.extend({});
@@ -7,7 +7,7 @@
     PersonalDetails.List = Backbone.Collection.extend({});
 
     PersonalDetails.Views.Edit = Backbone.View.extend({
-        
+
         tagName: 'div',
         className: 'personalDetails',
 
@@ -16,7 +16,7 @@
         },
 
         bindings: {
-        
+
             '#givenName': 'givenName',
             '#surname': 'surname',
             '#birthDate': {
@@ -24,17 +24,21 @@
 
                 // format date on model as ISO (YYYY-MM-DD)
                 onSet: function(val, options) {
-                    var dateArray = val.split("/");
-                    return new Date(dateArray[2] + '-'+ dateArray[1] + '-' + dateArray[0]);
+                    // var dateArray = val.split("/");
+                    var momentDate = moment(val, 'L', 'it')
+                    // return new Date(dateArray[2] + '-'+ dateArray[1] + '-' + dateArray[0]);
+                    return momentDate.format('YYYY-MM-DD')
                 },
 
                 // store data in view (from model) as DD/MM/YYYY (European format)
                 onGet: function(value, options) {
                     if (value) {
-                        var dateArray = value instanceof Date ? value.toISOString().split('-') : value.split('-');
+                        /*
+                        var dateArray = value instanceof Date ? value.toISOString().split('-') : moment(value).format('L');
                         var dateArray2 = dateArray[2].split('T');
                         dateArray[2] = dateArray2[0];
-                        return dateArray[2] + '/' + dateArray[1] + '/' + dateArray[0];
+                        return dateArray[2] + '/' + dateArray[1] + '/' + dateArray[0]; */
+                        return moment(value).lang("it").format('L');
                     }
                 },
 
@@ -42,7 +46,9 @@
                 initialize: function($el, model, options) {
                     var picker = new Pikaday({
                         field: $el[0],
-                        format: 'DD/MM/YYYY',
+                        // lang: 'it',
+                        // format: 'DD/MM/YYYY',
+                        format: moment.localeData('it')._longDateFormat.L,
                         minDate: moment('1900-01-01').toDate(),
                         maxDate: new Date()
                     });
