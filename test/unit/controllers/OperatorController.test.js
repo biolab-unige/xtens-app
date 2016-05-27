c96ed7012bfff29eb54d4ff218b9a45d6c66c1d2
nico.zanardi@gmail.com <nico.zanardi@gmail.com>
2016-05-27 11:13:14 +0200

    fixed test Operator controller change password

--- test/unit/controllers/OperatorController.test.js
+++ test/unit/controllers/OperatorController.test.js
@@ -23,52 +23,52 @@ describe('OperatorController', function() {
     describe('PATCH /updatePassword', function() {
         it('Should return OK 200, Password Updated', function(done) {
 
-          const admin = fixtures.operator[0];
-          const passport = _.find(fixtures.passport, {
-              'user': admin.id,
-              'protocol': 'local'});
+            const admin = fixtures.operator[0];
+            const passport = _.find(fixtures.passport, {
+                'user': admin.id,
+                'protocol': 'local'});
 
             request(sails.hooks.http.app)
-            .patch('/updatePassword')
+            .patch('/operator')
             .set('Authorization', `Bearer ${token}`)
             .send({
                 oldPass: passport.password,
                 newPass: "NewPassword",
                 cnewPass: "NewPassword"
-                })
+            })
             .expect(200, done);
         });
 
         it('Should return 400 bad Request, Old Password  Wrong', function(done) {
 
-          const admin = fixtures.operator[0];
+            const admin = fixtures.operator[0];
 
-          request(sails.hooks.http.app)
-          .patch('/updatePassword')
+            request(sails.hooks.http.app)
+          .patch('/operator')
           .set('Authorization', `Bearer ${token}`)
           .send({
-            oldPass: "WrongOldPass",
-            newPass: "NewPassword",
-            cnewPass: "NewPassword"
-              })
+              oldPass: "WrongOldPass",
+              newPass: "NewPassword",
+              cnewPass: "NewPassword"
+          })
           .expect(400, done);
-      });
-      it('Should return 400 bad Request, New Password and Confirm Confirm New Password do not match', function(done) {
+        });
+        it('Should return 400 bad Request, New Password and Confirm Confirm New Password do not match', function(done) {
 
-        const admin = fixtures.operator[0];
-        const passport = _.find(fixtures.passport, {
-            'user': admin.id,
-            'protocol': 'local'});
+            const admin = fixtures.operator[0];
+            const passport = _.find(fixtures.passport, {
+                'user': admin.id,
+                'protocol': 'local'});
 
-        request(sails.hooks.http.app)
-        .patch('/updatePassword')
+            request(sails.hooks.http.app)
+        .patch('/operator')
         .set('Authorization', `Bearer ${token}`)
         .send({
-          oldPass: passport.password,
-          newPass: "NewPassword",
-          cnewPass: "OtherNewPassword"
-            })
+            oldPass: passport.password,
+            newPass: "NewPassword",
+            cnewPass: "OtherNewPassword"
+        })
         .expect(400, done);
+        });
     });
-  });
 });
