9d530b4d99e0a149038114373906faf10431edb3
gicaz <nico.zanardi@gmail.com>
2017-09-13 17:51:08 +0200

    Defined isMultiProject method in SuperType Service. All tests pass #39

--- api/services/SuperTypeService.js
+++ api/services/SuperTypeService.js
@@ -1,11 +1,43 @@
 /**
- *  @author Massimiliano Izzo
+ *  @author Nicolò Zanardi
  */
 var BluebirdPromise = require('bluebird');
 
+const coroutines = {
+
+    /**
+     * @method
+     * @name isMultiProject
+     * @param{Object - Integer} SuperType object or id
+     * @return{Boolean}
+     * @description coroutine for get DataTypes' privileges
+     */
+    isMultiProject: BluebirdPromise.coroutine(function *(superType) {
+        if (!superType) {
+            throw new Error("isMultiProject - Error: No SuperType object");
+        }
+        const idSuperType = _.isObject(superType) ? superType.id : superType;
+
+        let dataTypes = yield DataType.find({superType: idSuperType});
+
+        // if (dataTypes.length === 0) {
+        //   return false;
+        // }
+        let isMultiProject = dataTypes.length > 1 ? true : false;
+        return isMultiProject;
+    })
+};
+
 var SuperTypeService = BluebirdPromise.promisifyAll({
 
+    isMultiProject: function (dataType, skipFieldsWithinLoops) {
 
+        return coroutines.isMultiProject(dataType, skipFieldsWithinLoops)
+        .catch(/* istanbul ignore next */ function(err) {
+            sails.log(err);
+            return err;
+        });
+    }
 
 });
 
